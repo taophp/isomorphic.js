b54905195b0f2bc57f5ab414d03357657687a8b8
Stéphane Mourey <mail@stephanemourey.fr>
2024-11-19 10:09:39 +0100

    Make the server listen on host and port defined in env

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,7 +1,18 @@
 import { load } from "jsr:@std/dotenv";
 import { route } from "jsr:@std/http/unstable-route";
-import * as shared from "../shared/main.ts";
 
 await load();
-console.log(Deno.env.get("APP_PATH"));
-Deno.serve(route(shared.getRoutes()), shared.defaultHandler);
+
+let app;
+const appPath = `../${Deno.env.get("APP_PATH") || "app.sample"}/main.ts`;
+try {
+  app = await import(appPath);
+} catch (error) {
+  console.error("Failed to load the application:", error);
+  Deno.exit(1);
+}
+
+const port = Number(Deno.env.get("PORT")) || 8000;
+const hostname = Deno.env.get("DOMAIN") || "localhost";
+
+Deno.serve({ hostname, port }, route(app.getRoutes()), app.defaultHandler);
